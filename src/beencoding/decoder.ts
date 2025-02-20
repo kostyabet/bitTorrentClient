b439ccb66fd3f0d2d8b664e1eb0373fb4b84df9c
Kostya Betenya <betenya.kostya@mail.ru>
2025-02-20 14:53:17 +0300

    fet: create read int and read string decode functino

--- src/beencoding/decoder.ts
+++ src/beencoding/decoder.ts
@@ -1,16 +1,18 @@
+import { Tokens } from "./tokens";
+const encoder = new TextEncoder();
 
 /**
  * Decodes a bencoded sequence of bytes.
 */
-class Decoder {
+export class Decoder {
 
     private _index: number;
-    private _data: string;
+    private _data: Int8Array;
 
     /**
-     * @data file info string data
+     * @data file info Int8Array data
     */
-    constructor(data: string) {
+    constructor(data: Int8Array) {
         this._index = 0;
         this._data = data;
     }
@@ -18,24 +20,49 @@ class Decoder {
     /**
      * Decodes the bencoded data and return the matching python object.
      * 
-     * @returns A python object representing the bencoded data.
+     * @returns An object representing the bencoded data.
     */
     decode() {
+        const char = this.peek();
+        
+        if (Tokens.digits.includes(char))
+            return this.decodeString();
 
+        switch (char) {
+            case (Tokens.integer): {
+                this.consume();
+                return this.decodeInt();
+            }
+            case (Tokens.list): {
+                this.consume();
+                return this.decodeList();
+            }
+            case (Tokens.dict): {
+                this.consume();
+                return this.decodeDict();
+            }
+            case (Tokens.end): {
+                return NaN;
+            }
+            default:
+                throw new Error("Unexpected end-of-file")
+        }
     }
 
     /**
      * Return the next character from the bencoded data or None.
     */
     peek() : number {
-        return new Int8Array([1])[0];
+        if (this._index >= this._data.length)
+            return NaN;
+        return this._data[this._index]
     }
 
     /**
      * Read (and therefore consume) the next character from the data.
     */
-    consume() : Int8Array {
-        return new Int8Array([1]);
+    consume() {
+        this._index++;
     }
 
     /**
@@ -43,8 +70,12 @@ class Decoder {
      * 
      * @length number of bytes.
     */
-    read(length: number) {
-
+    read(length: number) : Int8Array {
+        if (this._index + length > this._data.length)
+            throw new Error(`Cannot read ${length} bytes from current position ${this._index}!`);
+        const data = this._data.subarray(this._index, this._index + length);
+        this._index += length;
+        return data;
     }
 
     /**
@@ -52,12 +83,18 @@ class Decoder {
      * 
      * @tokens stop token.
     */
-    readUntil(token: Int8Array) {
-
+    readUntil(token: number) : number {
+        let num : number = 0;
+        do {
+            num = num * 10 + (this.peek() - encoder.encode('0')[0]);
+            this.consume();
+        } while (this.peek() != token)
+        this.consume();
+        return num;
     }
     
-    decodeInt() {
-
+    decodeInt() : number {
+        return this.readUntil(Tokens.end)
     }
 
     decodeList() {
@@ -69,6 +106,8 @@ class Decoder {
     }
 
     decodeString() {
-
+        const bytesToRead = this.readUntil(Tokens.strSeparator);
+        const data = this.read(bytesToRead);
+        return data;
     }
 }

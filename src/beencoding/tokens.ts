42e4b6020f57d577c54eeba3079e2f7feb669704
Kostya Betenya <betenya.kostya@mail.ru>
2025-02-20 13:40:29 +0300

    fix: summaty for tokens

--- src/beencoding/tokens.ts
+++ src/beencoding/tokens.ts
@@ -1,21 +1,28 @@
 const encoder = new TextEncoder();
 
-// <INT>   ::= "i"
-// Indicates start of integers
+/**
+ * Indicates start of integers
+ */
 const TOKEN_INTEGER = new Uint8Array(encoder.encode('i'));
 
-// <INT>   ::= "l"
-// Indicates start of list
+/**
+ * Indicates start of list
+*/
 const TOKEN_LIST = new Uint8Array(encoder.encode('l'));
 
-// <DICT>  ::= "d"
-// Indicates start of dict
+/**
+ * Indicates start of dict
+*/
 const TOKEN_DICT = new Uint8Array(encoder.encode('d'));
 
-// Indicate end of lists, dicts and integer values
+/**
+ * Indicate end of lists, dicts and integer values
+*/
 const TOKEN_END = new Uint8Array(encoder.encode('e'));
 
-// Delimits string length from string data
+/**
+ * Delimits string length from string data
+*/
 const TOKEN_STRING_SEPARATOR = new Uint8Array(encoder.encode(':'));
 
 export const Tokens = {

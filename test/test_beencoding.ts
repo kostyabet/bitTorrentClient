3df4e4bfa5b00a9630e1eeaab576c88a958a2513
Kostya Betenya <betenya.kostya@mail.ru>
2025-02-21 00:26:03 +0300

    fix: tests

--- test/test_beencoding.ts
+++ test/test_beencoding.ts
@@ -11,17 +11,17 @@ describe('Decode check', () => {
     });
 
     test('Try to parse list.', () => {
-        expect(new Decoder(new Int8Array(encoder.encode('l4:spam4:eggsi123ee'))).decode()).toBe([
-            [ 115, 112, 97, 109 ],
-            [ 101, 103, 103, 115 ],
+        expect(new Decoder(new Int8Array(encoder.encode('l4:spam4:eggsi123ee'))).decode()).toStrictEqual([
+            new Int8Array([ 115, 112, 97, 109 ]),
+            new Int8Array([ 101, 103, 103, 115 ]),
             123
         ]);
     });
 
     test('Try to parse dictionary.', () => {
         expect(new Decoder(new Int8Array(encoder.encode('d3:cow3:moo4:spam4:eggse'))).decode()).toStrictEqual(new Map<any, any>([
-            [[ 99, 111, 119 ], [ 109, 111, 111 ]],
-            [[ 115, 112, 97, 109 ], [ 101, 103, 103, 115] ]
+            [new Int8Array([ 99, 111, 119 ]), new Int8Array([ 109, 111, 111 ])],
+            [new Int8Array([ 115, 112, 97, 109 ]), new Int8Array([ 101, 103, 103, 115])]
         ]));
     });    
 });

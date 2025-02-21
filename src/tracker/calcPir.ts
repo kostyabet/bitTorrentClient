468d8384d385ea13babdfb0d31d14c4dc816eceb
Kostya Betenya <betenya.kostya@mail.ru>
2025-02-21 14:54:17 +0300

    fet: create calc peer id functoin

---
+++ src/tracker/calcPir.ts
@@ -0,0 +1,11 @@
+/**
+ * Calculate and return a unique Peer ID.
+ * 
+ * @returns '-PC0001-' + ''.join([str(random.randint(0, 9)) for _ in range(12)])
+ */
+export function calculatePeerId() {
+    const prefix = '-PC0001-';
+    const randomDigits = Array.from({ length: 12 }, () => Math.floor(Math.random() * 10))
+                              .join('');
+    return prefix + randomDigits;
+}

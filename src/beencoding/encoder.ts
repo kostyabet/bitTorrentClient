026341572b6fdda54184fea62de0d713a2ad96c1
Kostya Betenya <betenya.kostya@mail.ru>
2025-02-21 11:17:43 +0300

    fet: create encoder class

--- src/beencoding/encoder.ts
+++ src/beencoding/encoder.ts
@@ -1,5 +1,5 @@
-import { Tokens } from "./tokens";
 const encoder = new TextEncoder();
+const decoder = new TextDecoder();
 
 /**
  * Encodes an object to a bencoded sequence of bytes.
@@ -33,7 +33,52 @@ export class Encoder {
         return this.encodeNext(this._data)
     }
 
-    private encodeNext(data : any) {
-        console.log(typeof data)
+    private encodeNext(data : any) : Uint8Array {
+        switch(typeof data) {
+            case ("string"):
+                return this.encodeStr(data);
+            case ("number"):
+                return this.encodeInt(data);
+            case ("object"):
+                return this.encodeObject(data);
+            default: 
+                throw new Error("Unknown letter expected!")
+        }
+    }
+
+    private encodeStr(data : any) : Uint8Array {
+        const res = `${data.length}:${data}`;
+        return encoder.encode(res);
+    }
+
+    private encodeInt(data : any) : Uint8Array {
+        return encoder.encode('i' + data + 'e')
+    }
+
+    private encodeObject(data : any) : Uint8Array {
+        if (data instanceof Map)
+            return this.encodeMap(data);
+        return this.encodeArray(data);
+    }
+
+    private encodeMap(data : Map<any, any>) : Uint8Array {
+        let res = 'd';
+        data.forEach((key) => {
+            const encodeKey = this.encodeNext(key)
+            const encodeObj = this.encodeNext(data.get(key))
+            res += `${decoder.decode(encodeKey)}${decoder.decode(encodeObj)}`
+        });
+        res += 'e';
+        return encoder.encode(res);
+    }
+
+    private encodeArray(data : Int8Array) : Uint8Array {
+        let res = 'l';
+        data.forEach((el) => {
+            const arr : Uint8Array = this.encodeNext(el);
+            res += decoder.decode(arr)
+        });
+        res += 'e';
+        return encoder.encode(res);
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { Encoder } from "./beencoding/encoder"
 
-new Encoder('Hello').encode();
-new Encoder(123).encode();
-new Encoder(['123123', 123, '123']).encode();
-new Encoder(new Map<any,any>([
+console.log(new Encoder('Hello').encode());
+console.log(new Encoder(123).encode());
+console.log(new Encoder(['123123', 123, '123']).encode());
+console.log(new Encoder(new Map<any,any>([
     ['hello', 'hello'],
-])).encode();
+    ['hello', 'hello'],
+])).encode());

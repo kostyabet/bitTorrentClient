13da62dabc304cf19957475bd0f0e1eecc06aa82
Kostya Betenya <betenya.kostya@mail.ru>
2025-02-22 15:19:16 +0300

    fix: console.log miss

--- src/tracker/urlencode.ts
+++ src/tracker/urlencode.ts
@@ -4,6 +4,5 @@ export default function(params : Params) {
     const queryString = Object.keys(params)
             .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(String(params[key]))}`)
             .join('&');
-    console.log(queryString);
     return queryString;
 }
